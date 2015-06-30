f4c3d294acb9bdd45e241c06a55c0c267cc43245
water <water471871679@gmial.com>
2015-07-01 00:54:30 +0800

    Interview first commit

---
+++ interview/src/Interview.java
@@ -0,0 +1,16 @@
+/**
+ * Created by miao on 2015/7/1.
+ */
+public class Interview {
+    public static void main(String []args){
+        test1();
+    }
+
+    /**
+     * 面试题1:输出100%3.0的结果
+     * 答案为:1.0
+     */
+    private static void test1(){
+        System.out.println(100%3.0);
+    }
+}
